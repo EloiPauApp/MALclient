84b01682e74a4aabcd0488f7f715dd0cc506734d
pauITB <73183443+pauITB@users.noreply.github.com>
2021-03-08 15:57:33 +0100

    activity_login.xml creado

---
+++ app/src/main/java/com/example/malclient/verification/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.malclient.verification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.malclient.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
